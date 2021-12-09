8c7e2acdef2f94449e79054d3baf508d6ab73ff4
Namra Patel <namrajpatel@gmail.com>
2021-12-09 01:04:07 -0500

    asn5

--- client.py
+++ client.py
@@ -1,226 +1,54 @@
 import socket
-import os
-import signal
-import sys
-import argparse
-from urllib.parse import urlparse
-import selectors
+import struct
+import hashlib
 
-# Selector for helping us select incoming data from the server and messages typed in by the user.
+# Hard code location of the server.  Not what we'll want to be doing in the assignment,
+# but okay for an example like this.
 
-sel = selectors.DefaultSelector()
+UDP_IP = "localhost"
+UDP_PORT = 54321
 
-# Socket for sending messages.
+# Define a maximum string size for the text we'll be sending along.
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+MAX_STRING_SIZE = 256
 
-# User name for tagging sent messages.
+# The test data we want to be sending.  In this case, some text.
 
-user = ''
-
-# Signal handler for graceful exiting.  Let the server know when we're gone.
-
-def signal_handler(sig, frame):
-    print('Interrupt received, shutting down ...')
-    message=f'DISCONNECT {user} CHAT/1.0\n'
-    client_socket.send(message.encode())
-    sys.exit(0)
-
-# Simple function for setting up a prompt for the user.
-
-def do_prompt(skip_line=False):
-    if (skip_line):
-        print("")
-    print("> ", end='', flush=True)
-
-# Read a single line (ending with \n) from a socket and return it.
-# We will strip out any \r and \n in the process.
-
-def get_line_from_socket(sock):
-
-    done = False
-    line = ''
-    while (not done):
-        char = sock.recv(1).decode()
-        if (char == '\r'):
-            pass
-        elif (char == '\n'):
-            done = True
-        else:
-            line = line + char
-    return line
-
-
-# Collect information about file, setup a new file that will store the incoming packets from the server
-def recieve_file(words, sock):
-    # Ensure that recieved info has a length of 6
-    if len(words) == 6:
-        # Collect file information from the message recieved from the server
-        file_sender = words[1]
-        file_name = words[2]
-        file_size = words[3]
-        buffer_size = words[4]
-        buffer_size = int(buffer_size)
-        number_of_packets = words[5]
-        number_of_packets = int(number_of_packets)
-
-        # Print header information as specificed by assignment document
-        print(f'\nIncoming file: {file_name}')
-        print(f'Origin: {file_sender}')
-        print(f'Content-Length: {file_size}')
-
-        # Create a new file to store the recieved packets
-        file = open(file_name, 'w')
-        for i in range(0, number_of_packets):
-            recieved_packet = sock.recv(buffer_size).decode()
-            file.write(recieved_packet)
-        print('\nFile recieved, check current directory to view it.')
-
-    else:
-        print('Error: Invalid message from server, file not recieved.')
-        return
-
-
-# Read file from local storage, send server information about file size to prepare, send file in chunks to server
-def send_file(words, sock):
-    buffer_size = 4096 # 4KB
-    file_name = words[1]
-
-    file = open(file_name, 'r')
-    data=file.read() 
-    
-    # Get file size and find number of packets needed to send
-    file_size = len(data) 
-    number_of_packets = int(file_size//buffer_size)
-    if file_size % buffer_size > 0:
-        number_of_packets += 1
-
-    # Send the file metadata to the server so it knows how to setup to recieve the file
-    file_info = f'{file_size}, {buffer_size}, {number_of_packets}\n'
-    file_info = file_info.encode()
-    client_socket.send(file_info)
-
-    # Try sending the file to the server in chunks, if it fails, print an error to the client
-    try:
-        while data != '':
-            sent_packet = data[:buffer_size]    # queue up the next amount of bytes to be sent
-            sent_packet = sent_packet.encode()
-            client_socket.send(sent_packet)
-            data = data[buffer_size:] # begin to deplete the size of the file to keep track of what's been sent
-            print("File was sent to the server.")
-            do_prompt()
-    except Exception as e:    
-        print(e)
-        print("Error: File was not sent to the server.")
-        
-
-# Function to handle incoming messages from server.  Also look for disconnect messages to shutdown.
-
-def handle_message_from_server(sock, mask):
-    message=get_line_from_socket(sock)
-    words=message.split(' ')
-    print()
-    if words[0] == 'DISCONNECT':
-        print('Disconnected from server ... exiting!')
-        sys.exit(0)
-    elif words[0] == 'RECEIVE':
-        recieve_file(words, sock)
-    elif words[0] == 'SEND':
-        send_file(words, sock)
-    else:
-        print(message)
-        do_prompt()
-
-# Function to handle incoming messages from user.
-
-def handle_keyboard_input(file, mask):
-    line=sys.stdin.readline()
-    message = f'@{user}: {line}'
-    client_socket.send(message.encode())
-    do_prompt()
+TEST_DATA = "This is some test data.  Whee!"
 
 # Our main function.
 
-def main():
+def main(): 
 
-    global user
-    global client_socket
+    # Our packet is going to contain a sequence number, our data, the size of our data, and a checksum.
+    # Note that your packets may need to contain different things.
 
-    # Register our signal handler for shutting down.
+    sequence_number = 0
+    data = TEST_DATA.encode()
+    size = len(data)
+ 
+    # We now compute our checksum by building up the packet and running our checksum function on it.
+    # Our packet structure will contain our sequence number first, followed by the size of the data,
+    # followed by the data itself.  We fix the size of the string being sent ... as we are sending
+    # less data, it will be padded with NULL bytes, but we can handle that on the receiving end
+    # just fine!
 
-    signal.signal(signal.SIGINT, signal_handler)
+    packet_tuple = (sequence_number,size,data)
+    packet_structure = struct.Struct(f'I I {MAX_STRING_SIZE}s')
+    packed_data = packet_structure.pack(*packet_tuple)
+    checksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
 
-    # Check command line arguments to retrieve a URL.
+    # Now we can construct our actual packet.  We follow the same approach as above, but now include
+    # the computed checksum in the packet.
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("user", help="user name for this user on the chat service")
-    parser.add_argument("server", help="URL indicating server location in form of chat://host:port")
-    args = parser.parse_args()
+    packet_tuple = (sequence_number,size,data,checksum)
+    UDP_packet_structure = struct.Struct(f'I I {MAX_STRING_SIZE}s 32s')
+    UDP_packet = UDP_packet_structure.pack(*packet_tuple)
 
-    # Check the URL passed in and make sure it's valid.  If so, keep track of
-    # things for later.
+    # Finally, we can send out our packet over UDP and hope for the best.
 
-    try:
-        server_address = urlparse(args.server)
-        if ((server_address.scheme != 'chat') or (server_address.port == None) or (server_address.hostname == None)):
-            raise ValueError
-        host = server_address.hostname
-        port = server_address.port
-    except ValueError:
-        print('Error:  Invalid server.  Enter a URL of the form:  chat://host:port')
-        sys.exit(1)
-    user = args.user
-
-    # Now we try to make a connection to the server.
-
-    print('Connecting to server ...')
-    try:
-        client_socket.connect((host, port))
-    except ConnectionRefusedError:
-        print('Error:  That host or port is not accepting connections.')
-        sys.exit(1)
-
-    # The connection was successful, so we can prep and send a registration message.
-    
-    print('Connection to server established. Sending intro message...\n')
-    message = f'REGISTER {user} CHAT/1.0\n'
-    client_socket.send(message.encode())
-   
-    # Receive the response from the server and start taking a look at it
-
-    response_line = get_line_from_socket(client_socket)
-    response_list = response_line.split(' ')
-        
-    # If an error is returned from the server, we dump everything sent and
-    # exit right away.  
-    
-    if response_list[0] != '200':
-        print('Error:  An error response was received from the server.  Details:\n')
-        print(response_line)
-        print('Exiting now ...')
-        sys.exit(1)   
-    else:
-        print('Registration successful.  Ready for messaging!')
-
-    # Set up our selector.
-
-    client_socket.setblocking(False)
-    sel.register(client_socket, selectors.EVENT_READ, handle_message_from_server)
-    sel.register(sys.stdin, selectors.EVENT_READ, handle_keyboard_input)
-    
-    # Prompt the user before beginning.
-
-    do_prompt()
-
-    # Now do the selection.
-
-    while(True):
-        events = sel.select()
-        for key, mask in events:
-            callback = key.data
-            callback(key.fileobj, mask)    
-
-
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    sock.sendto(UDP_packet, (UDP_IP, UDP_PORT))
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,349 +1,70 @@
 import socket
-import os
-import signal
-import sys
-import selectors
+import struct
+import hashlib
 
-# Selector for helping us select incoming data and connections from multiple sources.
+# Hard code location of the server.  Not what we'll want to be doing in the assignment,
+# but okay for an example like this.
 
-sel = selectors.DefaultSelector()
+UDP_IP = "localhost"
+UDP_PORT = 54321
 
-# Client list for mapping connected clients to their connections.
+# Define a maximum string size for the text we'll be receiving.
 
-client_list = []
-
-# Dictionary of follow_lists
-
-dict_of_follow_lists = {}
-
-# Signal handler for graceful exiting.  We let clients know in the process so they can disconnect too.
-
-def signal_handler(sig, frame):
-    print('Interrupt received, shutting down ...')
-    message='DISCONNECT CHAT/1.0\n'
-    for reg in client_list:
-        reg[1].send(message.encode())
-    sys.exit(0)
-
-# Read a single line (ending with \n) from a socket and return it.
-# We will strip out the \r and the \n in the process.
-
-def get_line_from_socket(sock):
-
-    done = False
-    line = ''
-    while (not done):
-        char = sock.recv(1).decode()
-        if (char == '\r'):
-            pass
-        elif (char == '\n'):
-            done = True
-        else:
-            line = line + char
-    return line
-
-# Search the client list for a particular user.
-
-def client_search(user):
-    for reg in client_list:
-        if reg[0] == user:
-            return reg[1]
-    return None
-
-# Search the client list for a particular user by their socket.
-
-def client_search_by_socket(sock):
-    for reg in client_list:
-        if reg[1] == sock:
-            return reg[0]
-    return None
-
-# Add a user to the client list.
-
-def client_add(user, conn):
-    registration = (user, conn)
-    client_list.append(registration)
-    temp = "@all"
-    temp2 = "@"+user
-    client_follow_list = [temp, temp2]
-    dict_of_follow_lists[user] = client_follow_list
-
-# Remove a client when disconnected.
-
-def client_remove(user):
-    for reg in client_list:
-        if reg[0] == user:
-            client_list.remove(reg)
-            break
-
-# Loop through list of registered clients and return the list as a string.
-
-def get_client_list():
-    client_list_string = ""
-    for reg in client_list:
-        client_list_string += reg[0] + ", "
-    client_list_string = client_list_string[:-2]
-    return client_list_string
-
-# Loop through list of followed terms and return the list as a string.
-
-def get_follow_terms(user):
-    follow_list = ""
-    for item in dict_of_follow_lists[user]:
-        follow_list += item + ", "
-    follow_list = follow_list[:-2]
-    return follow_list
-
-# Implements a command manager that checks messages for commands and performs the respective actions.
-
-def command_manager(sock, words, user):
-    # Remove the ":" from the end of the user name string. 
-    words[0] = words[0][:-1]
-
-    # If !list is recieved, send the list of registered clients as a comma-separated string.
-    if words[1] == '!list':
-        returned_list = get_client_list()
-        forwarded_message = f'{returned_list}\n'
-        forwarded_message = forwarded_message.encode()
-        sock.send(forwarded_message)
-    
-    # If "!exit" is recieved, remove the user from the client list and close the connection.
-    elif words[1] == '!exit':
-        disconnection_msg = f'You will now be disconnected from the server.\n'
-        disconnection_msg = disconnection_msg.encode()
-        sock.send(disconnection_msg)
-        client_remove(user)
-        sel.unregister(sock)
-        sock.close()
-    
-    # If "!follow?" is recieved, send the list of followed items as a comma-separated string.
-    elif words[1] == '!follow?':
-        returned_list = get_follow_terms(user)
-        forwarded_message = f'{returned_list}\n'
-        forwarded_message = forwarded_message.encode()
-        sock.send(forwarded_message)
-    
-    # If "!follow" is recieved, add the term that follows to the list of followed terms.
-    elif words[1] == '!follow' and words[2] != None:
-        if (words[2] in dict_of_follow_lists[user]):
-            err_msg = f'Error: You are already following {words[2]}\n'
-            err_msg = err_msg.encode()
-            sock.send(err_msg)
-        else:            
-            dict_of_follow_lists[user].append(words[2])
-            success_msg = f'You are now following {words[2]}\n'
-            success_msg = success_msg.encode()
-            sock.send(success_msg)
-
-    # If "!unfollow" is recieved, remove the term that follows from the list of followed terms.
-    elif words[1] == '!unfollow':
-        
-        # If the user is not following the term, send an error message.
-        if words[2] not in dict_of_follow_lists[user]:
-            err_msg = f'Error: You are not following {words[2]}\n'
-            err_msg = err_msg.encode()
-            sock.send(err_msg)
-        
-        # Check that the terms the user is trying to remove are not "@all" and "@{user}".
-        if (words[2] == "@all" or words[2] == "@"+user):
-            err_msg = f'Error: You are not allowed to unfollow {words[2]}\n'
-            err_msg = err_msg.encode()
-            sock.send(err_msg)
-        
-        # Else, remove the term from the list of followed terms.
-        else:
-            dict_of_follow_lists[user].remove(words[2])
-            success_msg = f'You are no longer following {words[2]}\n'
-            success_msg = success_msg.encode()
-            sock.send(success_msg)
-        
-    # If "!attach" is recieved, process the file and send to the client
-    elif words[1] == '!attach':
-
-        if words[2] == None or words[3] == None:
-            err_msg = f'Error: !attach command is missing filename or followed terms.\n'
-            err_msg = err_msg.encode()
-            sock.send(err_msg)
-        try: 
-            # Request the file from the client
-            file_name = words[2]
-            file_request = f'SEND {file_name}\n'
-            sock.send(file_request.encode())
-            # Setup file info to be used to recieve the file
-            file_info = get_line_from_socket(sock).split(" ")
-            file_size = file_info[0]
-            file_size = int(file_size[:-1]) # Remove the "," from the end of the file size
-            buffer_size = file_info[1]
-            buffer_size = int(buffer_size[:-1]) # Remove the "," from the end of the buffer size
-            number_of_packets = file_info[2]
-            # Try building the file from recieved packets, send an error message if the file is not recieved correctly.
-            try: 
-                ready_file = ""
-                for i in range(0, int(number_of_packets)):
-                    incoming_packet = sock.recv(buffer_size).decode()
-                    ready_file = ready_file + incoming_packet
-            except:
-                err_msg = f'Error: File was not recieved correctly.\n'
-                err_msg = err_msg.encode()
-                sock.send(err_msg)
-
-            # Collect the list of subscribed terms the file was meant to be sent to
-            terms = words[3:]
-
-            # Try sending the file to the correct clients, send an error message if the file is not sent correctly.
-            try:
-                file_sender = user
-                for reg in client_list:
-                    # Ensure we do not send the file to the sender
-                    if reg[0] == file_sender:
-                        continue
-                    for item in dict_of_follow_lists[reg[0]]:
-                        if item in terms:
-                            file_recipient = reg[1]
-
-                            # Send file information to the file_recipient so it can build header 
-                            file_info = f'RECEIVE {file_sender} {file_name} {file_size} {buffer_size} {number_of_packets}\n'
-                            file_info.encode()
-                            file_recipient.send(file_info.encode())
-                            # Send packets to file_recipient
-                            fakevar = ready_file
-                            while ready_file != "":
-                                packet = fakevar[:buffer_size]
-                                packet = packet.encode()
-                                file_recipient.send(packet)
-                                fakevar = fakevar[buffer_size:]
-            except:
-                err_msg = f'Error: File was not sent correctly.\n'
-                err_msg = err_msg.encode()
-                sock.send(err_msg)
-                        
-        except Exception as e:
-            print(e)
-            err_msg = f'Error: File was not transferred correctly.\n'
-            err_msg = err_msg.encode()
-            sock.send(err_msg)
-
-    # Send the client an error if the command entered is not recognized.
-    else: 
-        err_msg = f'Error: Invalid command.\n'
-        err_msg = err_msg.encode()
-        sock.send(err_msg)
-
-# Function to read messages from clients.
-
-def read_message(sock, mask):
-    message = get_line_from_socket(sock)
-
-    # Does this indicate a closed connection?
-
-    if message == '':
-        print('Closing connection')
-        sel.unregister(sock)
-        sock.close()
-
-    # Receive the message.  
-
-    else:
-        user = client_search_by_socket(sock)
-        print(f'Received message from user {user}:  ' + message)
-        words = message.split(' ')
-        
-        # Check for client disconnection
-
-        if words[0] == 'DISCONNECT':
-            print('Disconnecting user ' + user)
-            client_remove(user)
-            sel.unregister(sock)
-            sock.close()
-
-        elif words[1].startswith('!'):
-            # Instaniate the command manager to find and handle all commands from the client.
-            command_manager(sock, words, user)
-
-        # Send message to all users who follow any term contained in the recieved message. Send at most only once, and don't send to yourself. 
-        # Need to re-add stripped newlines here.
-        else:
-            for reg in client_list:
-                if reg[0] == user:
-                    continue
-
-                # Check if the user is following any terms in the message, send the message to the user if they are.
-                for item in dict_of_follow_lists[reg[0]]:
-                    if item in words:
-                        client_socket = reg[1]
-                        forwarded_message = f'{message}\n'
-                        forwarded_message = forwarded_message.encode()
-                        client_socket.send(forwarded_message)
-                        break
-
-# Function to accept and set up clients.
-
-def accept_client(sock, mask):
-    conn, addr = sock.accept()
-    print('Accepted connection from client address:', addr)
-    message = get_line_from_socket(conn)
-    message_parts = message.split()
-
-    # Check format of request.
-
-    if ((len(message_parts) != 3) or (message_parts[0] != 'REGISTER') or (message_parts[2] != 'CHAT/1.0')):
-        print('Error:  Invalid registration message.')
-        print('Received: ' + message)
-        print('Connection closing ...')
-        response='400 Invalid registration\n'
-        conn.send(response.encode())
-        conn.close()
-
-    # If request is properly formatted and user not already listed, go ahead with registration.
-
-    else:
-        user = message_parts[1]
-
-        if (client_search(user) == None):
-            client_add(user,conn)
-            print(f'Connection to client established, waiting to receive messages from user \'{user}\'...')
-            response='200 Registration succesful\n'
-            conn.send(response.encode())
-            conn.setblocking(True)
-            sel.register(conn, selectors.EVENT_READ, read_message)
-
-        # If user already in list, return a registration error.
-
-        else:
-            print('Error:  Client already registered.')
-            print('Connection closing ...')
-            response='401 Client already registered\n'
-            conn.send(response.encode())
-            conn.close()
-
+MAX_STRING_SIZE = 256
 
 # Our main function.
 
-def main():
+def main(): 
 
-    # Register our signal handler for shutting down.
+    # Create our UDP socket and bind it to the address that clients expect us to be on.
 
-    signal.signal(signal.SIGINT, signal_handler)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    sock.bind((UDP_IP, UDP_PORT))
 
-    # Create the socket.  We will ask this to work on any interface and to pick
-    # a free port at random.  We'll print this out for clients to use.
+    # Next, we loop forever waiting for packets to arrive from clients.
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('', 0))
-    print('Will wait for client connections at port ' + str(server_socket.getsockname()[1]))
-    server_socket.listen(100)
-    server_socket.setblocking(False)
-    sel.register(server_socket, selectors.EVENT_READ, accept_client)
-    print('Waiting for incoming client connections ...')
-     
-    # Keep the server running forever, waiting for connections or messages.
-    
-    while(True):
-        events = sel.select()
-        for key, mask in events:
-            callback = key.data
-            callback(key.fileobj, mask)    
+    while True:
+
+        # We receive data and start to unpack it.  We'll use a 1024 byte buffer here.
+        # Notice that our packet structure mirrors what the client is sending along.
+        # The client and server have to agree on this or it won't work!
+
+        received_packet, addr = sock.recvfrom(1024)
+        unpacker = struct.Struct(f'I I {MAX_STRING_SIZE}s 32s')
+        UDP_packet = unpacker.unpack(received_packet)
+
+        # Extract out data that was received from the packet.  It unpacks to a tuple,
+        # but it's easy enough to split apart.
+
+        received_sequence = UDP_packet[0]
+        received_size = UDP_packet[1]
+        received_data = UDP_packet[2]
+        received_checksum = UDP_packet[3]
+
+        # Print out what we received.
+
+        print("Packet received from:", addr)
+        print("Packet data:", UDP_packet)
+
+        # We now compute the checksum on what was received to compare with the checksum
+        # that arrived with the data.  So, we repack our received packet parts into a tuple
+        # and compute a checksum against that, just like we did on the sending side.
+
+        values = (received_sequence,received_size,received_data)
+        packer = struct.Struct(f'I I {MAX_STRING_SIZE}s')
+        packed_data = packer.pack(*values)
+        computed_checksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+
+        # We can now compare the computed and received checksums to see if any corruption of
+        # data can be detected.  Note that we only need to decode the data according to the
+        # size we intended to send; the padding can be ignored.
+
+        if received_checksum == computed_checksum:
+            print('Received and computed checksums match, so packet can be processed')
+            received_text = received_data[:received_size].decode()
+            print(f'Message text was:  {received_text}')
+        else:
+            print('Received and computed checksums do not match, so packet is corrupt and discarded')
 
 if __name__ == '__main__':
     main()
-
